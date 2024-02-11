3eed0e7ab3a05c78a843bfb5bcfbdbeb40352a6f
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-02-11 18:44:06 +0200

    Add sticky header bottom border

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,4 +16,11 @@ window.onscroll = () => {
             })
         }
     })
+
+    // ========================== sticky header ==========================
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
 }
