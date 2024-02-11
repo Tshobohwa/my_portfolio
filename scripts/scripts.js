4f3774a283fb8592d8cc608ca85b1f11198e0eb3
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-02-11 21:48:30 +0200

    Add animations

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -39,3 +39,16 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 
 }
+
+// ========================== scroll reveal ==========================
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+})
+
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'})
+ScrollReveal().reveal('.home-img, services-container, .services-box, .portfolio-box, .contact form', {origin: 'bottom'})
+ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'})
+ScrollReveal().reveal('.about-content', {origin: 'right'})
